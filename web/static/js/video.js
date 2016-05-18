0eb60778c14a69e452422ed524bbd311306e2901
Dean Chouinard <deanchouinard@gmail.com>
2016-05-17 23:14:05 -0400

    sync annotations

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -28,12 +28,21 @@ let Video = {
       this.renderAnnotation(msgContainer, resp)
     })
 
+    msgContainer.addEventListener("click", e => {
+      e.preventDefault()
+      let seconds = e.target.getAttribute("data-seek") ||
+                    e.target.parentNode.getAttribute("data-seek")
+      if(!seconds){ return }
+
+      Player.seekTo(seconds)
+    })
+
     vidChannel.join()
     //  .receive("ok", resp => console.log("joined the video channel", resp))
-      .receive("ok", ({annotations}) => {
-        annotations.forEach( ann => this.renderAnnotation(msgContainer, ann) ) }) 
+      .receive("ok", resp => {
+        this.scheduleMessages(msgContainer, resp.annotations)
+      })
       .receive("error", reason => console.log("join failed", reason) )
-
   },
 
   esc(str){
@@ -44,16 +53,40 @@ let Video = {
 
   renderAnnotation(msgContainer, {user, body, at}){
     let template = document.createElement("div")
-      
     template.innerHTML = `
      <a href="#" data-seek="${this.esc(at)}">
+     [${this.formatTime(at)}]
      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
      </a>
       `
     msgContainer.appendChild(template)
     msgContainer.scrollTop = msgContainer.scrollHeight
-  }
-    // vidChannel.on("ping", ({count}) => console.log("PING", count) )
+  },
+
+  scheduleMessages(msgContainer, annotations){
+    setTimeout(() => {
+      let ctime = Player.getCurrentTime()
+      let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+      this.scheduleMessages(msgContainer, remaining)
+    }, 1000)
+  },
+
+  renderAtTime(annotations, seconds, msgContainer){
+    return annotations.filter( ann => {
+      if(ann.at > seconds){
+        return true
+      } else {
+        this.renderAnnotation(msgContainer, ann)
+        return false
+      }
+    })
+  },
+
+  formatTime(at){
+    let date = new Date(null)
+      date.setSeconds(at / 1000)
+      return date.toISOString().substr(14, 5)
+  },
 }
 
 export default Video
