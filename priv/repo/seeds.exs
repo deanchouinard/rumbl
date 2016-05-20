77a077258d4e704ffdca911becb20fdbc633c6a7
Dean Chouinard <deanchouinard@gmail.com>
2016-05-20 17:09:50 -0400

    integrating otp services and channels

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,9 +12,16 @@
 
 alias Rumbl.Repo
 alias Rumbl.Category
+alias Rumbl.User
+import Ecto.Query
 
 for category <- ~w(Action Drama Romance Comedy Sci-fi) do
   Repo.get_by(Category, name: category) ||
     Repo.insert!(%Category{name: category})
 end
 
+
+user_params = %{name: "Wolfran", username: "wolfram", password: "wolfram"}
+changeset = User.registration_changeset(%User{}, user_params)
+Repo.get_by(User, username: "wolfram") ||
+  Repo.insert!(changeset)
